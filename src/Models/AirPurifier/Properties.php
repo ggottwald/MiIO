f09bb91dbcecf3f4edb4b621c0a8eb73ec5d54f7
Götz Gottwald <goetz.gottwald@gmx.net>
2018-04-26 21:30:53 +0200

    UPDATE add param

--- src/Models/AirPurifier/Properties.php
+++ src/Models/AirPurifier/Properties.php
@@ -9,76 +9,80 @@ namespace MiIO\Models\AirPurifier;
  */
 class Properties implements \JsonSerializable
 {
-	public $power;
-	public $mode;
-	public $temp_dec;
-	public $humidity;
-	public $aqi;
-	public $favorite_level;
-	public $filter1_life;
-	public $f1_hour_used;
-	public $use_time;
-	public $led;
-	public $led_b;
-	public $buzzer;
+    public $power;
+    public $mode;
+    public $temp_dec;
+    public $humidity;
+    public $aqi;
+    public $favorite_level;
+    public $filter1_life;
+    public $f1_hour_used;
+    public $use_time;
+    public $led;
+    public $led_b;
+    public $buzzer;
+    public $purify_volume;
+    public $learn_mode;
 
-	protected $attributes = [
-		'power',
-		'mode',
-		'temp_dec',
-		'humidity',
-		'aqi',
-		'favorite_level',
-		'filter1_life',
-		'f1_hour_used',
-		'use_time',
-		'led',
-		'led_b',
-		'buzzer',
-	];
+    protected $attributes = [
+        'power',
+        'mode',
+        'temp_dec',
+        'humidity',
+        'aqi',
+        'favorite_level',
+        'filter1_life',
+        'f1_hour_used',
+        'use_time',
+        'led',
+        'led_b',
+        'buzzer',
+        'purify_volume',
+        'learn_mode',
+    ];
 
-	public function __construct($attributes = [])
-	{
-		$this->fill($attributes);
-	}
+    public function __construct($attributes = [])
+    {
+        $this->fill($attributes);
+    }
 
-	/**
-	 * @return array
-	 */
-	public function getAttributes()
-	{
-		return $this->attributes;
-	}
+    /**
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
 
-	/**
-	 * @param array $attributes
-	 */
-	public function fill($attributes)
-	{
-		foreach ($attributes as $key => $value) {
-			if (property_exists($this, $key)) {
-				$this->$key = $value;
-			}
-		}
-	}
+    /**
+     * @param array $attributes
+     */
+    public function fill($attributes)
+    {
+        foreach ($attributes as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
 
-	/**
-	 * @param string $name
-	 * @return bool
-	 */
-	public function __isset($name)
-	{
-		return $this->$name !== null;
-	}
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return $this->$name !== null;
+    }
 
-	public function jsonSerialize()
-	{
-		$attributeList = [];
+    public function jsonSerialize()
+    {
+        $attributeList = [];
 
-		foreach ($this->attributes as $attribute) {
-			$attributeList[$attribute] = $this->$attribute;
-		}
+        foreach ($this->attributes as $attribute) {
+            $attributeList[$attribute] = $this->$attribute;
+        }
 
-		return $attributeList;
-	}
+        return $attributeList;
+    }
 }
